b96ad4d935de1feb7aa1a990f8272d85776918d5
YutaSanada <yuta_sanada@timebank.kp>
2019-10-15 20:29:34 +0900

    SearchLocationのView追加

---
+++ sample/SearchLocation/SearchLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchLocationViewController.swift
+//  sample
+//
+//  Created by Sanada Yuta on 2019/10/15.
+//  Copyright © 2019 Sanada Yuta. All rights reserved.
+//
+
+import UIKit
+
+class SearchLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
